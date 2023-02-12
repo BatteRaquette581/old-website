2a4b4b5709c2a1c38963ebea0629ddee7dfa3bbe
BatteRaquette581 <84451047+BatteRaquette581@users.noreply.github.com>
2023-02-12 18:04:52 +0000

    Removing code that doesn't change anything (src/js/index.js)

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,1 @@
-let navBar = document.querySelector("nav");
-let navBarHeight = navBar.clientHeight;
-
-document.querySelectorAll(".icon-link").forEach(icon => {
-    let iconWidth = icon.clientWidth;
-    let iconHeight = icon.clientHeight;
-    icon.parentNode.style.marginLeft = (document.clientWidth - iconWidth) / 2 + "px";
-    icon.parentNode.style.marginTop = (navBarHeight - iconHeight) / 2 + "px";
-});
+// unused

517dfe7c15a95de30ab6d6b8940b3b015b290503
BatteRaquette581 <84451047+BatteRaquette581@users.noreply.github.com>
2023-02-12 18:02:56 +0000

    Made index.html, discord.html and changed indentation to 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-// useless for now
+let navBar = document.querySelector("nav");
+let navBarHeight = navBar.clientHeight;
+
+document.querySelectorAll(".icon-link").forEach(icon => {
+    let iconWidth = icon.clientWidth;
+    let iconHeight = icon.clientHeight;
+    icon.parentNode.style.marginLeft = (document.clientWidth - iconWidth) / 2 + "px";
+    icon.parentNode.style.marginTop = (navBarHeight - iconHeight) / 2 + "px";
+});

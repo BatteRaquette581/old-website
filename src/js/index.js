51d0e1bfb6d308e26b85865b5b36148af76f4ecf
BatteRaquette581 <84451047+BatteRaquette581@users.noreply.github.com>
2023-02-13 16:21:17 +0000

    Center icons horizontally in the navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-// unused
+const navBar = document.querySelector("nav");
+const navHeight = navBar.clientHeight;
+
+document.querySelectorAll(".icon-link").forEach(icon => {
+    let marginTop = (navHeight - icon.clientHeight) / 2;
+    icon.style.marginTop = marginTop + "px";
+});
